310885223522209b6528cc39a22ee682e714f00d
Brandon Del Mundo <brandon.delmundo@gmail.com>
2026-08-12 15:35:06 -0700

    Added functionality that allows for the collection of start, end, and building data specifically for the center for humanities event page. Will look to see if it can be used for other instances

--- news.py
+++ news.py
@@ -24,16 +24,33 @@ class NewsChecker(Checker):
         events = contents.find_all(class_="views-row")
 
         for event in events:
-            title = event.find("a").get_text(strip=True) if event.find("a") is not None else None
+            title = event.find("h2").get_text(strip=True) if event.find("a") is not None else None
             start = event.find(class_="date-display-single").get("content") if event.find(class_="date-display-single") is not None else None
 
             url = event.find("a").get("href") if event.find("a") is not None else None
             url = f"{page_url}{url}" if url is not None else None
+
+            request = requests.get(url, headers=headers)
+            soup = BeautifulSoup(request.text,"html.parser")
+            end = None
+            if start is None:
+                start = soup.find(class_='date-display-start').get('content') if soup.find(class_='date-display-start') else None
+                start = soup.find(class_='date-display-single').get('content') if soup.find(class_='date-display-start') is None else start
+                end = soup.find(class_='date-display-end').get('content') if soup.find(class_='date-display-end') else None
+
+            location = None
+            if soup.find(class_='field-type-addressfield') is not None:
+                street = soup.find(class_='street-block').get_text(strip=True)
+                address = soup.find(class_='addressfield-container-inline').get_text()
+                country = soup.find(class_='country').get_text(strip=True)
+                location = f"{street}, {address}, {country}" if street and address and country else None
 
             self.events.append(Event(
                 source_url=self.source_url,
                 source_type=self.source_type,
                 title=title,
                 start=start,
+                end=end,
+                building=location,
                 url=url
             ))
